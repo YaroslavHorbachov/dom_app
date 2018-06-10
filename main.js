cf162b794e0dc49eaed4a67c420586e05ff044db
ZavodPodushek <new_mouse@mail.ru>
2018-06-10 20:43:23 +0300

    change script

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const $ = document.querySelector;
 
-const input = $('input')
-const init = (fn) => input.addEventListener('input', fn)
-const listener = (event) => console.log(event.target.value)
-init(listener) 
+const input = $('input');
+const init = (fn) => input.addEventListener('input', fn);
+const listener = (event) => console.log(event.target.value);
+
+init(listener);

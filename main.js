9a2a9e2abc4e18100005ec81afdcff49d6eb1608
ZavodPodushek <new_mouse@mail.ru>
2018-06-10 20:39:07 +0300

    Initialize app

---
+++ main.js
@@ -0,0 +1,6 @@
+const $ = document.querySelector;
+
+const input = $('input')
+const init = (fn) => input.addEventListener('input', fn)
+const listener = (event) => console.log(event.target.value)
+init(listener) 
